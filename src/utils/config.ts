f691228013d36d2bd4288fe72530ce48462e3d29
hem <makhmutov_tagir@bk.ru>
2023-06-07 22:20:51 +0300

    Init

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://exec-5d59.onrender.com"
+export const API_URL = process.env.REACT_APP_SERVER
 export const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/iu
