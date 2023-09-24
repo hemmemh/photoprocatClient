3d21faf7e743ec3ef3ebf44df3b0ce20507fd2d5
hememh <makhmutov_tagir@bk.ru>
2023-09-24 15:32:16 +0300

    new2

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 8080
+const app = express()
+app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static(__dirname))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,"build",'index.html'))
+})
+
+app.listen(PORT)
