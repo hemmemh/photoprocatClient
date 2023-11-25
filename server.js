7b450ca922ce0b4881824caf36a37ba6c609f5aa
hememh <makhmutov_tagir@bk.ru>
2023-11-25 19:23:24 +0300

    3e

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.static(path.resolve(__dirname, 'build')))
-app.use(express.static(__dirname))
+
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,"build",'index.html'))
+    res.sendFile(path.resolve(__dirname,"build",'index.html'))
 })
 
 app.listen(PORT)
