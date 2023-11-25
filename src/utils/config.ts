b87eb638be744f014737dc9054a9a5c665f62977
hemmemh <makhmutov_tagir@bk.ru>
2023-11-25 19:48:10 +0300

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://31.129.107.38:5000'
+export const API_URL = 'https://photoprocat-server-rusz.vercel.app'
 export const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 export const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/iu
